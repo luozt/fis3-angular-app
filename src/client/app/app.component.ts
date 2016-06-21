c876d847af817f9a78a328d1481afaa9b59f245a
luozt <luozt@foxmail.com>
2016-06-21 13:25:03 +0800

    配置fis和初步的ng2程序

---
+++ src/client/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'sd-app',
+  templateUrl: __uri('app.component.html')
+})
+export class AppComponent {
+}
---
+++ src/client/index.js
@@ -0,0 +1,8 @@
+// @require fis-mod
+
+require('zone.js');
+require('reflect-metadata');
+
+require('./app/main.ts');
+
+
